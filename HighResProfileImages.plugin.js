b86ebcab3a9e4f3bcd1ff0b40c3ab20afbe05f02
Z'ark Ashveil <mewmewby2@gmail.com>
2026-05-02 00:38:35 -0700

    Move constants inside `module.exports`

--- HighResProfileImages.plugin.js
+++ HighResProfileImages.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name High-Res Profile Images
- * @version 1.0.0
+ * @version 1.0.1
  * @author Z'ark Ashveil
  * @authorId 262113677900120065
  * @authorLink https://github.com/Kawtious
@@ -9,33 +9,33 @@
  * @updateUrl https://raw.githubusercontent.com/Kawtious/HighResProfileImages/main/HighResProfileImages.plugin.js
  */
 
-const PLUGIN_ID = "HighResProfileImages";
-
-const VALID_SIZES = [
-    256,
-    512,
-    1024,
-    2048,
-    4096
-]
-
-const DEFAULT_SETTINGS = {
-    resSize: VALID_SIZES[0]
-}
-
 module.exports = class HighResProfileImages {
+    PLUGIN_ID = "HighResProfileImages";
+
+    VALID_SIZES = [
+        256,
+        512,
+        1024,
+        2048,
+        4096
+    ]
+
+    DEFAULT_SETTINGS = {
+        resSize: this.VALID_SIZES[0]
+    }
+
     constructor() {
-        this.settings = BdApi.Data.load(PLUGIN_ID, "settings");
+        this.settings = BdApi.Data.load(this.PLUGIN_ID, "settings");
 
         if (!this.settings) {
-            this.settings = DEFAULT_SETTINGS;
-            BdApi.Data.save(PLUGIN_ID, "settings", this.settings);
+            this.settings = this.DEFAULT_SETTINGS;
+            BdApi.Data.save(this.PLUGIN_ID, "settings", this.settings);
         }
     }
 
     start() {
         BdApi.Patcher.after(
-            PLUGIN_ID,
+            this.PLUGIN_ID,
             BdApi.Webpack.getModule(m => m?.getUserAvatarURL),
             "getUserAvatarURL",
             (_, __, res) => this.replaceSizeParam(res, this.settings.resSize)
@@ -43,13 +43,13 @@ module.exports = class HighResProfileImages {
     }
 
     stop() {
-        BdApi.Patcher.unpatchAll(PLUGIN_ID);
+        BdApi.Patcher.unpatchAll(this.PLUGIN_ID);
     }
 
     getSettingsPanel() {
         const loadWarning = "Warning: The higher the value, the more resources will be used";
 
-        const sizeOptions = VALID_SIZES.map(size => { return { label: size, value: size } })
+        const sizeOptions = this.VALID_SIZES.map(size => { return { label: size, value: size } })
 
         const resSize = {
             id: "resSize",
@@ -64,7 +64,7 @@ module.exports = class HighResProfileImages {
             settings: [resSize],
             onChange: (_, id, value) => {
                 this.settings[id] = value;
-                BdApi.Data.save(PLUGIN_ID, "settings", this.settings);
+                BdApi.Data.save(this.PLUGIN_ID, "settings", this.settings);
             }
         });
     }
@@ -72,7 +72,7 @@ module.exports = class HighResProfileImages {
     replaceSizeParam(url, size) {
         if (!url || typeof url !== "string") return url;
 
-        if (!VALID_SIZES.includes(size)) return url;
+        if (!this.VALID_SIZES.includes(size)) return url;
 
         if (!/[?&]size=/.test(url))
             return url + (url.includes("?") ? "&" : "?") + `size=${size}`;
