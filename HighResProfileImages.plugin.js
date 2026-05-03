402830211b48dfd57ff4f4641722c29de2a9f279
Z'ark Ashveil <mewmewby2@gmail.com>
2026-05-03 12:29:23 -0700

    Improve code readability

--- HighResProfileImages.plugin.js
+++ HighResProfileImages.plugin.js
@@ -9,70 +9,72 @@
  * @updateUrl https://raw.githubusercontent.com/Kawtious/HighResProfileImages/main/HighResProfileImages.plugin.js
  */
 
+const PLUGIN_ID = "HighResProfileImages";
+
+const VALID_SIZES = [
+    256,
+    512,
+    1024,
+    2048,
+    4096
+]
+
+const DEFAULT_SETTINGS = {
+    resSize: VALID_SIZES[0]
+}
+
 module.exports = class HighResProfileImages {
-    PLUGIN_ID = "HighResProfileImages";
-
-    VALID_SIZES = [
-        256,
-        512,
-        1024,
-        2048,
-        4096
-    ]
-
-    DEFAULT_SETTINGS = {
-        resSize: this.VALID_SIZES[0]
-    }
-
     constructor() {
-        this.settings = BdApi.Data.load(this.PLUGIN_ID, "settings");
+        this.settings = BdApi.Data.load(PLUGIN_ID, "settings");
 
         if (!this.settings) {
-            this.settings = this.DEFAULT_SETTINGS;
-            BdApi.Data.save(this.PLUGIN_ID, "settings", this.settings);
+            this.settings = DEFAULT_SETTINGS;
+            BdApi.Data.save(PLUGIN_ID, "settings", this.settings);
         }
     }
 
     start() {
+        // Patch everything that fetches the user's avatar
         BdApi.Patcher.after(
-            this.PLUGIN_ID,
+            PLUGIN_ID,
             BdApi.Webpack.getModule(m => m?.getUserAvatarURL),
             "getUserAvatarURL",
-            (_, __, res) => this.replaceSizeParam(res, this.settings.resSize)
+            (_, __, returnValue) => this.patchUserAvatar(returnValue)
         );
     }
 
     stop() {
-        BdApi.Patcher.unpatchAll(this.PLUGIN_ID);
+        BdApi.Patcher.unpatchAll(PLUGIN_ID);
     }
 
     getSettingsPanel() {
         const resSize = {
             id: "resSize",
-            name: "Resolution Size",
+            name: "Avatar Size",
             note: "Warning: The higher the value, the more resources will be used",
             type: "dropdown",
             value: this.settings.resSize,
-            options: this.VALID_SIZES.map(size => { return { label: size, value: size } })
+            options: VALID_SIZES.map(size => { return { label: size, value: size } })
         };
 
         return BdApi.UI.buildSettingsPanel({
             settings: [resSize],
             onChange: (_, id, value) => {
                 this.settings[id] = value;
-                BdApi.Data.save(this.PLUGIN_ID, "settings", this.settings);
+                BdApi.Data.save(PLUGIN_ID, "settings", this.settings);
             }
         });
     }
 
-    replaceSizeParam(url, size) {
+    patchUserAvatar(url) {
         if (!url || typeof url !== "string") return url;
 
-        if (!this.VALID_SIZES.includes(size)) return url;
+        if (!VALID_SIZES.includes(this.settings.resSize)) return url;
 
+        // Add 'size' parameter if URL doesn't have it
         if (!/[?&]size=/.test(url))
-            return url + (url.includes("?") ? "&" : "?") + `size=${size}`;
+            return url + (url.includes("?") ? "&" : "?") + `size=${this.settings.resSize}`;
 
-        return url.replace(/([?&])size=\d+/g, `$1size=${size}`);
+        return url.replace(/([?&])size=\d+/g, `$1size=${this.settings.resSize}`);
     }
 };
